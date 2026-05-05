3164b8c73ab96e5fe276657f3b330feb253fc7d9
ahtisham-ali-1999 <aliahtisham241@gmail.com>
2026-05-05 16:09:00 +0500

    initial commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import HeroSection from "./HeroSection";
+import HeroSection from "./Herosection";
 import projectData from "../data/projectData";
 import { Link } from "react-router-dom";
 import HtmlLogo from './../assets/html-logo.png';
